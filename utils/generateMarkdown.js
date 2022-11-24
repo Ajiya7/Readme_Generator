60b4e29771d85c986282fd6c81274553bdbfbd73
Faruk Ajiya <Ajiyafaruk@gmail.com>
2022-11-24 03:52:53 -0800

    Markdown completed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,44 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.Title}
 
+  ![License](https://img.shields.io/badge/license-${data.license}-blue "License Badge")
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installastion](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contribution)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## Description
+  ${data.Description}
+
+  ## Installation
+  ${data.Installation_instructions}
+
+  ## Usage
+  ${data.Usage_info}
+
+  ## License
+  Notice: For more information about the license, click on the link below.
+  ![License link](https://choosealicense.com/licenses/${data.license}/)
+
+  ## Contribution
+  ${data.Contribution}
+
+  ## Test
+  ${data.Test_instructions}
+
+  ## Questions
+  For questions about this project you can go to my GitHub page at the following link:
+  - [GitHub Profile](https://github.com/${data.Username})
+
+  For additional questions please reach out to me: ${data.Email}.
 `;
 }
 
